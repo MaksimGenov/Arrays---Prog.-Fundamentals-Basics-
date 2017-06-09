16342eb739f381f2bdfb608fb66e42b11eee74ab
Maximus <maximgenov@abv.bg>
2017-06-09 21:13:10 +0300

    Upload 06. Max Sequence of Equal Elements

---
+++ 06. Max Sequence of Equal Elements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _06.Max_Sequence_of_Equal_Elements
+{
+    class MaxSequenceOfEqualElements
+    {
+        static void Main()
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            var bestStart = 0;
+            var longestLenght = 0;
+            var MatchCount = 0;
+
+            var endOfLoop = arr.Length;
+
+            for (int i = 0; i < endOfLoop; i++)
+            {
+                for (int j = i; j < endOfLoop; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        MatchCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (MatchCount > longestLenght)
+                {
+                    longestLenght = MatchCount;
+                    bestStart = i;
+                }
+                MatchCount = 0;
+            }
+
+            for (int i = bestStart; i < (bestStart + longestLenght); i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}

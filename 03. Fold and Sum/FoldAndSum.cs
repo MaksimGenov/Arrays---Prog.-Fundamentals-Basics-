b08ca07ce17eb8ca1214e4b88fcbdafc928f7598
Maximus <maximgenov@abv.bg>
2017-06-10 22:16:15 +0300

    Optimized solution for 03. Fold and Sum/FoldAndSum.cs

--- 03. Fold and Sum/FoldAndSum.cs
+++ 03. Fold and Sum/FoldAndSum.cs
@@ -8,21 +8,20 @@ namespace _03.Fold_and_Sum
         {
             int[] numbers =
                 Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
             var k = numbers.Length / 4;
 
-            int[] leftNumbers = new int[k];
-            int[] rightNumbers = new int[k];
-            int[] middleNumbers = new int[2 * k];
             int[] finalResult = new int[2 * k];
 
             for (int i = 0; i < k; i++)
             {
                 finalResult[0 + i] = numbers[k - 1 - i] + numbers[k + i];
-                finalResult[(2 * k - 1) - i] = numbers[(3 * k - 1) - i] + numbers[3 * k + i];
+
+                finalResult[(2 * k - 1) - i] = 
+                    numbers[(3 * k - 1) - i] + numbers[3 * k + i];
             }
 
             Console.WriteLine(string.Join(" ", finalResult));
-
 
         }
     }
